d03b73b48044db80a595d03112f074748b5dc1ba
Ashour100 <interinstitute5@gmail.com>
2022-02-22 16:37:32 +0100

    Minor change

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.getElementById("km").innerHTML =`Devi percorrere ${km} chilometri`;
 document.getElementById("eta").innerHTML =`Hai ${eta} anni`;
 
 if(eta<18||eta>65)
-document.getElementById("prezzo_O").innerHTML =`Il prezzo Originale: ${prezzo}`;
+document.getElementById("prezzo_O").innerHTML =`Il prezzo Originale: ${prezzo.toFixed(2)}&euro;`;
 
 if(eta<18){
     prezzo-=((prezzo/100)*20);
@@ -14,4 +14,4 @@ else if(eta>65){
     prezzo-=((prezzo/100)*40);
 }
 
-document.getElementById("prezzo_F").innerHTML =`Il prezzo Finale: ${prezzo}`;
+document.getElementById("prezzo_F").innerHTML =`Il prezzo Finale: ${prezzo.toFixed(2)}&euro;`;
