58e29404dc3bd8a44f111cc14a09657eb02abd70
Ashour100 <interinstitute5@gmail.com>
2022-02-22 14:42:26 +0100

    Fatto

---
+++ main.js
@@ -0,0 +1,17 @@
+let km=parseFloat(prompt("Quanti chilometri vuoi percorrere"));
+let eta=parseInt (prompt("Quanti anni hai"));
+let prezzo= km * 0.21;
+document.getElementById("km").innerHTML =`Devi percorrere ${km} chilometri`;
+document.getElementById("eta").innerHTML =`Hai ${eta} anni`;
+
+if(eta<18||eta>65)
+document.getElementById("prezzo_O").innerHTML =`Il prezzo Originale: ${prezzo}`;
+
+if(eta<18){
+    prezzo-=((prezzo/100)*20);
+}
+else if(eta>65){
+    prezzo-=((prezzo/100)*40);
+}
+
+document.getElementById("prezzo_F").innerHTML =`Il prezzo Finale: ${prezzo}`;
